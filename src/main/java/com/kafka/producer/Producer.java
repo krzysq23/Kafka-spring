935da41fc6d4df2adfe7203635d0d045a8b7e6c7
krzysq23 <krzysztof.skarbowski@gmail.com>
2020-01-07 13:32:29 +0100

    Producer + Consumer

---
+++ src/main/java/com/kafka/producer/Producer.java
@@ -0,0 +1,29 @@
+package com.kafka.producer;
+
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@Component
+public class Producer {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private static Gson gson = new GsonBuilder().create();
+
+	public void send() {
+		Message message = new Message();
+		message.setId("KFK_" + System.currentTimeMillis());
+		message.setMsg(UUID.randomUUID().toString());
+		message.setSendTime(new Date());
+		kafkaTemplate.send("test", gson.toJson(message));
+	}
+
+}
